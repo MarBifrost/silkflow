627798cc422faeec4eb96e47cd537b1797276654
MarBifrost <128312989+MarBifrost@users.noreply.github.com>
2026-01-13 17:39:24 +0400

    minor changes in main logic

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, request, jsonify
 from flask_mysqldb import MySQL
+import MySQLdb.cursors
 from auth import auth_bp # Import the blueprint
 
 app = Flask(__name__)
@@ -22,5 +23,20 @@ def index():
         return render_template('main.html', email=session['email'])
     return redirect(url_for('auth.login'))
 
+@app.route('/main')
+def main_page():
+    if request.method == "GET":
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+
+        query = 'SELECT shifts.shift_date, employees.name FROM shifts join employees on shifts.employee_id = employee.id;'
+        cursor.execute(query)
+        shifts = cursor.fetchall()
+        cursor.close()
+
+        for shift in shifts:
+            if shift['shift_date']:
+                shift['shift_date'] = shift['shift_date'].strftime('%Y-%m-%d')
+
+        return jsonify(shifts)
 if __name__ == '__main__':
     app.run(debug=True)
